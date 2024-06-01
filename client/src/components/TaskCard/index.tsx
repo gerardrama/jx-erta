925a257bdfb1466ad2fa87793d5c7faf1d7d27aa
Fredi <fredigjoni70@gmail.com>
2024-06-01 17:17:11 +0200

    Added card ribbon

--- client/src/components/TaskCard/index.tsx
+++ client/src/components/TaskCard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './TaskCard.module.css';
-import { Avatar, Card, Modal, Tooltip } from 'antd';
+import { Avatar, Badge, Card, Modal, Tooltip } from 'antd';
 import {UserOutlined, AntDesignOutlined} from '@ant-design/icons';
 
 const TaskCard = ({provided, snapshot, getItemStyle}) => {
@@ -21,32 +21,34 @@ const TaskCard = ({provided, snapshot, getItemStyle}) => {
                 <p>Some contents...</p>
                 <p>Some contents...</p>
             </Modal>
-            <Card 
-                ref={provided.innerRef}
-                {...provided.draggableProps}
-                {...provided.dragHandleProps}
-                style={getItemStyle(
-                    snapshot.isDragging,
-                    provided.draggableProps.style
-                )}
-                className={styles.card}
-                title="Card title"
-                onClick={showModal}
-            >
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-                <div className={styles.avatars}>
-                    <Avatar.Group maxCount={2} maxStyle={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>
-                        <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=2" />
-                        <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
-                        <Tooltip title="Ant User" placement="top">
-                            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
-                        </Tooltip>
-                        <Avatar style={{ backgroundColor: '#1677ff' }} icon={<AntDesignOutlined />} />
-                    </Avatar.Group>
-                </div>
-            </Card>
+                
+                <Card 
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                    )}
+                    className={styles.card}
+                    title="Card title"
+                    onClick={showModal}
+                    >
+                    <Badge.Ribbon rootClassName={styles.ribbon} text="Critical" color="red"></Badge.Ribbon>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                    <div className={styles.avatars}>
+                        <Avatar.Group maxCount={2} maxStyle={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>
+                            <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=2" />
+                            <Avatar style={{ backgroundColor: '#f56a00' }}>F</Avatar>
+                            <Tooltip title="Ant User" placement="top">
+                                <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+                            </Tooltip>
+                            <Avatar style={{ backgroundColor: '#1677ff' }} icon={<AntDesignOutlined />} />
+                        </Avatar.Group>
+                    </div>
+                </Card>
         </>
     );
 };
